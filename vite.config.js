8be28d1e6c29e61c3e245219fbc0eb4bcbb4b9e9
catcooks <javerbenito@gmail.com>
2026-05-04 15:21:33 +0800

    ll

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Add this
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/catcooks/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   plugins: [
     react(),
     tailwindcss(), // Add this
